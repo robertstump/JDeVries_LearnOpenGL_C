81733a7c5e02e680c9693d7a76f857fff9f4b823
Rob Stump <robertstump@gmail.com>
2025-03-20 07:10:18 -0400

    working 3d camera

--- launch.sh
+++ launch.sh
@@ -2,14 +2,14 @@ echo "##########################################################"
 echo "#             Compiling OPENGL Testing....               #"
 echo "##########################################################"
 
-clang -std=c99 -o bin/coordEx src/coordinate.c -I/opt/homebrew/include -L/opt/homebrew/lib -Iinclude -lglfw -framework Cocoa -framework OpenGL -framework IOKit -DGL_SILENCE_DEPRECATION
+clang -std=c99 -o bin/cameraSetupEx src/cameraSetup.c -I/opt/homebrew/include -L/opt/homebrew/lib -Iinclude -lglfw -framework Cocoa -framework OpenGL -framework IOKit -DGL_SILENCE_DEPRECATION
 
 if [ $? -eq 0 ]; then
     echo "Compile complete...."
     echo "##########################################################"
     echo "#             Running OPENGL Testing....                 #"
     echo "##########################################################"
-    bin/coordEx
+    bin/cameraSetupEx
 
 else
     echo "Compilation failed."
