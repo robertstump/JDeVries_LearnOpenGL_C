16a8c9e2daa365de6da6bf1496151fa27ff62850
Rob Stump <robertstump@gmail.com>
2025-03-23 18:27:32 -0400

    built camera, finish starter chapters

--- src/cameraFree.c
+++ src/cameraFree.c
@@ -11,7 +11,6 @@
 #include "shaders.h"
 #include "textures.h"
 #include "linmath.h"
-#include "camera.h"
 
 #define ARENA_SIZE 1024 * 1024
 #define SCREEN_WIDTH 1280
@@ -21,16 +20,28 @@
 #define HALF_SCREEN_HEIGHT 480
 #define TRUE 1
 #define FALSE 0
+#define DEG2RAD(degrees) ((degrees) * M_PI / 180.f) 
 
 float mixOffset;
+vec3 cameraPos = {0.f, 0.f, 3.f};
+vec3 cameraUp = {0.f, 1.f, 0.f};
+vec3 cameraFront = {0.f, 0.f, -3.f};
 float deltaTime;
 float lastFrame;
 float currentFrame;
+float yaw, pitch;
+float lastX, lastY;
 int mouseFirst;
-Camera *cam;
+float zoom;
 
 void scroll_callback(GLFWwindow *window, double xOffet, double yOffset) {
-    ProcessMouseScroll(cam, yOffset);
+    zoom -= (float) yOffset;
+    if(zoom < 1.f) {
+        zoom = 1.f;
+    }
+    if(zoom > 45.f) {
+        zoom = 45.f;
+    }
 }
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
@@ -66,36 +77,75 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 }
 
 void processInput(GLFWwindow *window) {
+    float cameraSpeed = 5.f * deltaTime;
     
     if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        ProcessKeyboard(cam, FORWARD, deltaTime);
+        vec3 frontSpeed;
+        vec3_scale(frontSpeed, cameraFront, cameraSpeed);
+        vec3_add(cameraPos, cameraPos, frontSpeed);
     }
 
    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) { 
-       ProcessKeyboard(cam, BACKWARD, deltaTime);
-   }
+        vec3 frontSpeed;
+        vec3_scale(frontSpeed, cameraFront, cameraSpeed);
+        vec3_sub(cameraPos, cameraPos, frontSpeed);
+    }
 
    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) { 
-        ProcessKeyboard(cam, LEFT, deltaTime); 
-   }
+        vec3 frontUp;
+        vec3_mul_cross(frontUp, cameraFront, cameraUp);
+        vec3_norm(frontUp, frontUp);
+        vec3_scale(frontUp, frontUp, cameraSpeed);
+        vec3_add(cameraPos, cameraPos, frontUp);
+    }
 
-  if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) { 
-       ProcessKeyboard(cam, RIGHT, deltaTime);
-   }
+   if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) { 
+        vec3 frontUp;
+        vec3_mul_cross(frontUp, cameraFront, cameraUp);
+        vec3_norm(frontUp, frontUp);
+        vec3_scale(frontUp, frontUp, cameraSpeed);
+        vec3_sub(cameraPos, cameraPos, frontUp);
+    }
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     if(mouseFirst) {
-        cam->lastX = xpos;
-        cam->lastY = ypos;
+        lastX = xpos;
+        lastY = ypos;
         mouseFirst = FALSE;
     }
 
-    float xOffset = xpos - cam->lastX;
-    float yOffset = cam->lastY - ypos;
-    cam->lastX = xpos;
-    cam->lastY = ypos;
-    ProcessMouseMovement(cam, xOffset, yOffset, TRUE); 
+    float xOffset = xpos - lastX;
+    float yOffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.1f;
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    yaw += xOffset;
+    pitch += yOffset;
+
+    if(pitch > 89.f) {
+        pitch = 89.f;
+    }
+    
+    if(pitch < -89.f) {
+        pitch = - 89.f;
+    }
+
+    vec3 direction;
+    float radYaw = DEG2RAD(yaw);
+    float radPitch = DEG2RAD(pitch);
+    float cosRadYaw = cosf(radYaw);
+    float sinRadYaw = sinf(radYaw);
+    float cosRadPitch = cosf(radPitch);
+    float sinRadPitch = sinf(radPitch);
+    direction[0] = cosRadYaw * cosRadPitch;
+    direction[1] = sinRadPitch;
+    direction[2] = sinRadYaw * cosRadPitch;
+    vec3_norm(cameraFront, direction);
 }
 
 void error_callback(int error, const char* description) {
@@ -114,13 +164,12 @@ int main() {
     int width = SCREEN_WIDTH;
     int height = SCREEN_HEIGHT;
     mixOffset = .4f;
+    float lastX = HALF_SCREEN_WIDTH, lastY = HALF_SCREEN_HEIGHT;
     mouseFirst = TRUE;
+    yaw = -90.f;
+    zoom = 45.f;
 
     ScratchArena arena = createArena(ARENA_SIZE);
-    arenaPush(&arena);
-    cam = arenaAlloc(&arena, sizeof(Camera), sizeof(float));
-    
-    CameraInit(cam, (vec3){0.f, 0.f, 0.f}, (vec3){0.f, 1.f, 0.f}, YAW, PITCH);
 
     if(!glfwInit()) {
         fprintf(stderr, "ERROR: Failed to initialzie GLFW.\n");
@@ -199,7 +248,7 @@ int main() {
          { -3.8f, -2.f, -12.3f },
          { 2.4f, -0.4f, -3.5f },
          { -1.7f, 3.f, -7.5f },
-         { 1.3f, -2.0f, -2.5f },
+         { 1.3f, 2.0f, -2.5f },
          { 1.5f, 2.f, -2.5f },
          { 1.5f, 0.2f, -1.5f },
          { -1.3f, 1.f, -1.5f }
@@ -236,18 +285,34 @@ int main() {
     glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
     glUniform1i(glGetUniformLocation(shader.ID, "texture2"), 1);
     
-    //init screen transformation matrices
     mat4x4 projection;
     mat4x4_identity(projection);
     float fovRad;
+    
     mat4x4 model;
     mat4x4_identity(model);
+
+    vec3 cameraTarget = {0.f, 0.f, 0.f};
+    
+    vec3 cameraDirection;
+    vec3_sub(cameraDirection, cameraPos, cameraTarget);
+    vec3_norm(cameraDirection, cameraDirection);
+
+    vec3 cameraRight;
+    vec3 up = {0.f, 1.f, 0.f};
+    vec3_mul_cross(cameraRight, up, cameraDirection);
+    vec3_norm(cameraRight, cameraRight);
+
+    vec3_mul_cross(cameraUp, cameraRight, cameraDirection);
+
     mat4x4 view;
+    //mat4x4_look_at(view, cameraPos, cameraTarget, up);
     
     GLuint modelLoc = glGetUniformLocation(shader.ID, "model");
     GLuint projLoc = glGetUniformLocation(shader.ID, "projection");
     GLuint viewLoc = glGetUniformLocation(shader.ID, "view");
    
+    float radius = 10.f;
     float zRot;
 
     while(!glfwWindowShouldClose(window)) {
@@ -256,18 +321,18 @@ int main() {
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        fovRad = DEG2RAD(cam->Zoom);
+        fovRad = DEG2RAD(zoom);
 
         processInput(window);
 
+        vec3_add(cameraTarget, cameraPos, cameraFront);
+        mat4x4_look_at(view, cameraPos, cameraTarget, up);
+
         zRot = (float)glfwGetTime();
 
-        mat4x4_perspective(projection, fovRad, (float)width / height, 0.1f, 100.f); 
-        mat4x4_rotate(model, model, 1.f, 1.f, 0.f, zRot);
+        mat4x4_perspective(projection, fovRad, width / height, 0.1f, 100.f); 
         
-        vec3 tmp;
-        vec3_add(tmp, cam->Position, cam->Front);
-        mat4x4_look_at(view, cam->Position, tmp, cam->Up);
+        mat4x4_rotate(model, model, 1.f, 1.f, 0.f, zRot);
         
         glClearColor(0.2f, 0.3f, 0.6f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -301,7 +366,7 @@ int main() {
         }
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }
+        }
     
     glDeleteTextures(1, &texture1);
     glDeleteTextures(1, &texture2);
