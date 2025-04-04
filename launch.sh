8b5d16a036b542ceac72163e4020ba2eb84db68f
Rob Stump <robertstump@gmail.com>
2025-04-03 21:33:11 -0400

    launch script update for test and dbg modes

--- launch.sh
+++ launch.sh
@@ -2,17 +2,81 @@ echo "##########################################################"
 echo "#             Compiling OPENGL Testing....               #"
 echo "##########################################################"
 
-clang -std=c99 -g -Wall -Werror -o bin/lighting2EX src/lighting2.c -I/opt/homebrew/include -L/opt/homebrew/lib -Iinclude -lglfw -framework Cocoa -framework OpenGL -framework IOKit -DGL_SILENCE_DEPRECATION
+DEBUG_FLAGS="-fsanitize=address -g -O0 -Wall -Werror"
+LIGHT_DBG_FLAGS="-g -Wall -Werror"
+CFLAGS=$LIGHT_DBG_FLAGS
+TEST_ONLY=false
 
-if [ $? -eq 0 ]; then
-    echo "Compile complete...."
-    echo "##########################################################"
-    echo "#             Running OPENGL Testing....                 #"
-    echo "##########################################################"
-    bin/lighting2EX
+#for arg in "$@"; do
+    #if [ "$arg" = "-d" ] || [ "$arg" = "--debug" ]; then
+        #echo "[!] Debug mode enabled."
+        #CFLAGS=$DEBUG_FLAGS
+    #fi  
+#done
 
-else
-    echo "Compilation failed."
+for arg in "$@"; do 
+    case "$arg" in 
+        -d|--debug)
+            echo "[!] Debug mode enable."
+            CFLAGS=$DEBUG_FLAGS
+            ;;
+        -t|--test-only)
+            echo "[!] Running in test-only mode."
+            TEST_ONLY=true;;
+        *)
+            echo "Syntax: launch.sh [flag][flag]"
+            echo "Flags --debug, -d, --test-only, -t"
+            ;;
+    esac
+done
+
+if [ "$TEST_ONLY" = true ]; then
+    echo
+    echo "##########################################################"
+    echo "#                   Running Unit Tests....               #"
+    echo "##########################################################"
+
+    if ! ./runtests.sh; then
+        echo "[ ] Unit tests failed."
+        exit 1
+    fi
+
+echo "[X] All tests passed."
+exit 0
+
+fi
+
+SRC_DIR="src"
+INCLUDE_DIR="include"
+BIN_DIR="bin"
+TARGET="$BIN_DIR/lighting2Ex"
+
+SRC_MAIN="$SRC_DIR/lighting2.c"
+#SRC_SECONDARY="SRC_DIR/..."
+
+clang -std=c99 $LIGHT_DBG_FLAGS -o $TARGET $SRC_MAIN -I/opt/homebrew/include -L/opt/homebrew/lib -Iinclude -lglfw -framework Cocoa -framework OpenGL -framework IOKit -DGL_SILENCE_DEPRECATION
+
+if [ $? -ne 0 ]; then
+    echo "[ ] Compilation Failed."
     exit 1
 fi
 
+echo "[X] Main Compilation Complete"
+echo 
+echo "##########################################################"
+echo "#                   Running Unit Tests....               #"
+echo "##########################################################"
+
+if ! ./runtests.sh; then
+    echo "[ ] Unit tests failed."
+    exit 1
+fi
+
+echo "[X] All tests passed."
+
+echo "##########################################################"
+echo "#             Running OPENGL Testing....                 #"
+echo "##########################################################"
+echo
+
+./$TARGET
